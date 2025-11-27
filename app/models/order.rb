7981deac78ce68fc38df223fa7df82a787bf2bf4
Julián Pinzón Eslava <julian@julianpinzon.com>
2025-11-27 16:31:13 +1100

    remove refreshes

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,6 @@ class Order < ApplicationRecord
   belongs_to :customer
   enum :status, %i[received preparing packing ready_for_pickup delivered canceled].index_by(&:itself)
 
-  broadcasts_refreshes_to ->(_) { "dashboard" }
-
   scope :by_id, ->(id) { where(id:) }
   scope :by_status, ->(status) { where(status:) }
   scope :by_customer_name, ->(name) { joins(:customer).where(customers: { name: }) }
